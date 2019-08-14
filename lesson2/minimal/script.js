90ce83e7d98f4d604574f84e241f513fa1b0e8a0
Aleksandr <alexinc2.0@gmail.com>
2019-08-14 19:36:31 +0300

    minimal

--- lesson2/minimal/script.js
+++ lesson2/minimal/script.js
@@ -1,30 +1,55 @@
-var names = [
+
+var users = [
     {
         firstName: "Александр",
         secondName: "Македонский",
         thirdName: "Цезаревич",
-        avatar: "https://i.pinimg.com/736x/29/a8/20/29a82067b71bd9e3df95e1c0ba5c4daf--fantasy-art-avatar-jake-sully.jpg"
+        avatar: "http://nice-avatar.ucoz.ru/_ph/4/2/933339845.png"
     },
     {
         firstName: "Максим",
         secondName: "Авросский",
         thirdName: "Наимович",
-        avatar: "https://i.pinimg.com/736x/29/a8/20/29a82067b71bd9e3df95e1c0ba5c4daf--fantasy-art-avatar-jake-sully.jpg"
+        avatar: ""
     },
     {
         firstName: "Платон",
         secondName: "Планетный",
         thirdName: "Меркурович",
-        avatar: "https://i.pinimg.com/736x/29/a8/20/29a82067b71bd9e3df95e1c0ba5c4daf--fantasy-art-avatar-jake-sully.jpg"
+        avatar: "https://s017.radikal.ru/i427/1303/35/85bea17b0018.gif"
+    },
+    {
+        firstName: "Карл",
+        secondName: "Фиджеральд",
+        thirdName: "Карлович",
+        avatar: "https://cheat-master.ru/avatar/04/859171.png"
+    },
+    {
+        firstName: "Морис",
+        secondName: "Бирюза",
+        thirdName: "Агапович",
+        avatar: "http://smm-blogs.ru/upload/000/u1/036/ca01410e.jpg"
     }
 ];
 
-var vw = new Vue({
+var app = new Vue({
     el: '#app',
     data: function(){
         return {
-            title: 'Список имен',
-            names: names
+            isVisible: false,
+            users: users,
+            defaultAvatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaUD8jlvVjouynDln66CRBRL-ecagVV5I3xhRBXJweSU_1iyYLTw'
+        }
+    },
+    computed: {
+        fullName() {
+            return this.users.firstName + this.users.secondName;
+        },
+        title(){
+            return 'Всего человек: ' + this.users.length;
+        },
+        ava(user){
+            return user.avatar != '' ? user.avatar : defaultAvatar;
         }
     }
 });
